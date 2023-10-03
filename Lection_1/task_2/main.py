befb5066d2949c9e6494b1a2ce8deb52a58dcfb8
yurikkon <yuriy.doss@gmail.com.com>
2023-10-03 21:12:30 +0300

    add lection 1 task 2

---
+++ Lection_1/task_2/main.py
@@ -0,0 +1,3 @@
+first_number = int(input("Write first number: "))
+second_number = int(input("Write second number: "))
+print("here is the sum of these two numbers: " + str(first_number + second_number))
