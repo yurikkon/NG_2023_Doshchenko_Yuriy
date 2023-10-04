8fe5549c70f38f31494bf3e39d0617f5d525ca59
yurikkon <yuriy.doss@gmail.com.com>
2023-10-04 18:11:28 +0300

    remade lection 1 task 2

--- Lection_1/task_2/main.py
+++ Lection_1/task_2/main.py
@@ -1,3 +1,3 @@
-first_number = int(input("Write first number: "))
-second_number = int(input("Write second number: "))
+first_number = int(input("Enter the first number to find the amount: "))
+second_number = int(input("Enter the second number to find the amount: "))
 print("here is the sum of these two numbers: " + str(first_number + second_number))
