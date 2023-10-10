456f28640f26ed6a378adfcb278ea425d4ee99f4
yurikkon <yuriy.doss@gmail.com.com>
2023-10-10 19:00:02 +0300

    remade lection 1 task 3

--- Lection_1/task_3/main.py
+++ Lection_1/task_3/main.py
@@ -1,13 +1,11 @@
-choise = int(input("Write 1 if you want to convert degrees to fahrenheit\nWrite 2 if you want to convert fahrenheit to degrees: "))
+choise = int(input("Write 1 if you want to convert degreesCelcius to fahrenheit\nWrite 2 if you want to convert fahrenheit to degrees Celcius: "))
 
 
 if choise == 1:
-    degrees = float(input("write how many degrees you want to convert: "))
+    degrees = float(input("write how many degrees Celcius you want to convert: "))
     fahrenheit = (degrees * 1.8) + 32
     print("that's how much it will be in Fahrenheit: " + str(fahrenheit))
-
-
-else:
+elif choise == 2:
     fahrenheit = float(input("write how many fahrenheit you want to convert: "))
     degrees = (fahrenheit - 32) / 1.8
-    print("that's how much it will be in degrees: " + str(degrees))
+    print("that's how much it will be in degrees Celcius: " + str(degrees))
